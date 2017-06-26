fb42a82918859d9560bc371bf6ef13201d1b02a8
Barry-Crawford <barryacrawford@yahoo.com>
2017-06-25 23:14:49 -0400

    Add files via upload

---
+++ ioVarsLab.py
@@ -0,0 +1,18 @@
+age = 39
+name = "Barry Crawford"
+gpa = 3.82
+operand1 = 978.425
+operand2 = 0.0324
+
+print "I am {} years old.".format(age)
+print "My name is {} ".format(name)
+print "In my school my GPA was {}".format(gpa)
+
+print "{} + {} = {}".format(operand1, operand2, operand1+operand2)
+print "{} - {} = {}".format(operand1, operand2, operand1-operand2)
+print "{} * {} = {}".format(operand1, operand2, operand1*operand2)
+print "{} / {} = {}".format(operand1, operand2, operand1/operand2)
+
+
+print "{} % {} = {}".format(9,3, 9 % 3)
+print "{} % {} = {}".format(112,10, 112 % 10) 
